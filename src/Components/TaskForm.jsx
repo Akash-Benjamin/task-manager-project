48b4d5c977419db2d7c93fcea44902b65451bc84
Akash-Benjamin <benjaminakash9343@gmail.com>
2026-03-03 21:58:23 +0530

    task and some JS added

--- src/Components/TaskForm.jsx
+++ src/Components/TaskForm.jsx
@@ -1,9 +1,30 @@
 import React from "react"
+import { useState } from "react"
 
 export default function TaskForm() {
+    const [task, setTask,] = useState('');
+    const { priority, setPriority } = useState('Medium')
+    const { category, setCategory } = useState('General')
     return (
-        <div>
-            <h2>Task Form</h2>
-        </div>
+        <form className="task-form">
+            <div id="inp">
+                <input type="text" placeholder="Enter the task"
+                    onChange={(e) => setTask(e.target.value)} />
+                <span><button type="submit">ADD</button></span>
+                <h1>{task}</h1>
+            </div>
+            <div id="btns">
+                <select>
+                    <option value="">high</option>
+                    <option value="">Medium</option>
+                    <option value="">Low</option>
+                </select>
+                <select>
+                    <option value="">General</option>
+                    <option value="">Work</option>
+                    <option value="">Personal</option>
+                </select>
+            </div>
+        </form>
     )
 }
