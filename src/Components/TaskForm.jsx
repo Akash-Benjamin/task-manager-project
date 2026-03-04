f25e6490ab15ca3d2753a0a98b858f2415435cd5
Akash-Benjamin <benjaminakash9343@gmail.com>
2026-03-04 21:37:16 +0530

    added some functionalities to the form

--- src/Components/TaskForm.jsx
+++ src/Components/TaskForm.jsx
@@ -2,27 +2,26 @@ import React from "react"
 import { useState } from "react"
 
 export default function TaskForm() {
-    const [task, setTask,] = useState('');
-    const { priority, setPriority } = useState('Medium')
-    const { category, setCategory } = useState('General')
+    const [task, setTask] = useState('');
+    const [priority, setPriority] = useState('Medium')
+    const [category, setCategory] = useState('General')
     return (
         <form className="task-form">
             <div id="inp">
                 <input type="text" placeholder="Enter the task"
                     onChange={(e) => setTask(e.target.value)} />
                 <span><button type="submit">ADD</button></span>
-                <h1>{task}</h1>
             </div>
             <div id="btns">
-                <select>
-                    <option value="">high</option>
-                    <option value="">Medium</option>
-                    <option value="">Low</option>
+                <select onChange={(e) => setPriority(e.target.value)}>
+                    <option value="High">High</option>
+                    <option value="Medium">Medium</option>
+                    <option value="Low">Low</option>
                 </select>
-                <select>
-                    <option value="">General</option>
-                    <option value="">Work</option>
-                    <option value="">Personal</option>
+                <select onChange={(e) => setCategory(e.target.value)}>
+                    <option value="General">General</option>
+                    <option value="Work">Work</option>
+                    <option value="Personal">Personal</option>
                 </select>
             </div>
         </form>
