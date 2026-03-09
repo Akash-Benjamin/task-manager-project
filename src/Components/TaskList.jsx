903773f40d78f852b1d5c82baca84b3487aa5ed8
Akash-Benjamin <benjaminakash9343@gmail.com>
2026-03-09 21:08:46 +0530

    list function added sec 2

--- src/Components/TaskList.jsx
+++ src/Components/TaskList.jsx
@@ -1,8 +1,21 @@
 import React from "react";
-export default function Tasklist() {
+export default function Tasklist(tasks, updateTask, deleteTask) {
     return (
-        <div>
-            <h2>Task List</h2>
-        </div>
+        <ul className="task-list">{
+            task.map(({ task, index }) => (
+                <li>
+                    <div>
+                        <span>{task.text}</span>
+                        <small>{task.category}, {task.priority}</small>
+                    </div>
+                    <div>
+                        <button onClick={() => toggleCompletes(index)}
+                        >{task.completed ? "undo" : "complete"}</button>
+                        <button>Delete</button>
+                    </div>
+                </li>
+            ))
+        }
+        </ul>
     )
 }
