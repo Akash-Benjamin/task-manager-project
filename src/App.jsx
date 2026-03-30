7acee0a63bf5ca5edfa50042fb541a0a0b4360e1
Akash-Benjamin <benjaminakash9343@gmail.com>
2026-03-30 22:07:45 +0530

    list completed and undo button fuctionality added

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React, { useEffect } from "react";
 import TaskForm from "./Components/TaskForm.jsx";
 import Tasklist from "./Components/TaskList";
 import ProgressTracker from "./Components/ProgressTracker";
+import "./style.css"
 import { useState } from "react"
 
 export default function App() {
@@ -21,17 +22,21 @@ export default function App() {
   const deleteTask = (index) => {
     setTasks(tasks.filter((_, i) => i != index))
   }
+  const clearTasks = () => {
+    setTasks([]);
+  }
   return (
-    <div>
+    <div className="App">
       <header>
-        <h1>Task Manager</h1>
-        <p>your friendly task Manager</p>
+        <h1 className="title">Task Manager</h1>
+        <p className="tagline">your friendly task Manager</p>
       </header>
       <TaskForm addTask={addTask} />
       <Tasklist tasks={tasks}
         updateTask={updateTask}
         deleteTask={deleteTask} />
       <ProgressTracker tasks={tasks} />
+      {tasks.length > 0 && (<button className="clear-btn" onClick={clearTasks}>Clear All Task</button>)}
     </div>
   )
 }
